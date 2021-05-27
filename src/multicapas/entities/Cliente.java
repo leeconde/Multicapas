53337896af6163326d376fa1bf1e479ac3d4810f
leeconde <lecondesouza@gmail.com>
2021-05-26 22:22:11 -0300

    Entidade Cliente criada

---
+++ src/multicapas/entities/Cliente.java
@@ -0,0 +1,90 @@
+package multicapas.entities;
+
+import java.io.Serializable;
+
+public class Cliente implements Serializable {
+
+    private int id;
+
+    private String nomeLoja;
+
+    private String cidade;
+
+    private String endereço;
+
+    private String telefone;
+
+    private String nomeResponsavel;
+
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(int id, String nomeLoja, String cidade, String endereço, String telefone, String nomeResponsavel, String email) {
+        this.id = id;
+        this.nomeLoja = nomeLoja;
+        this.cidade = cidade;
+        this.endereço = endereço;
+        this.telefone = telefone;
+        this.nomeResponsavel = nomeResponsavel;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeLoja() {
+        return nomeLoja;
+    }
+
+    public void setNomeLoja(String nomeLoja) {
+        this.nomeLoja = nomeLoja;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNomeResponsavel() {
+        return nomeResponsavel;
+    }
+
+    public void setNomeResponsavel(String nomeResponsavel) {
+        this.nomeResponsavel = nomeResponsavel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
