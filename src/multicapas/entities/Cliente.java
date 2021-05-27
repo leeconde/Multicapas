204699d8d572fa640bdc2db914714a65eab74761
leeconde <lecondesouza@gmail.com>
2021-05-26 23:43:06 -0300

    persistence.xml configurado, entidade mapeada

--- src/multicapas/entities/Cliente.java
+++ src/multicapas/entities/Cliente.java
@@ -1,9 +1,16 @@
 package multicapas.entities;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Cliente implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String nomeLoja;
